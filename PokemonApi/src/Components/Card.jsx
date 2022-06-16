fd905d69f5eaf94d7a69a0d50f16d4e4f085f830
Rahamtullah sheikh <76489436+rhm856591@users.noreply.github.com>
2022-06-16 15:44:17 +0530

    Add files via upload

---
+++ PokemonApi/src/Components/Card.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+const Card = ({ pokemon, loading,infoPokemon}) => {
+   // console.log(pokemon);
+    return (
+        <>
+        {
+            loading ? <h1>Loading...</h1> :
+                pokemon.map((item) => {
+                    console.log(item);
+                    return (
+                        <>
+                            <div className="card" key={item.id} onClick={()=>infoPokemon(item)}>
+                                <h2>{item.id}</h2>
+                                <img src={item.sprites.front_default} alt="" />
+                                <h2>{item.name}</h2>
+                            </div>
+                        </>
+                    )
+                })
+        }
+
+        </>
+    )
+}
+export default Card;
